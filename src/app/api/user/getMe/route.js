7d8aed07d69d98058cd8166562c9bdd8f83e9570
Proglov <dev.mohammad.parvizi@gmail.com>
2024-10-12 12:14:15 +0330

    added the profile page

---
+++ src/app/api/user/getMe/route.js
@@ -0,0 +1,36 @@
+import { getUser } from '@/lib/getUser'
+import prisma from '@/lib/prismaDB'
+import { NextResponse } from 'next/server'
+
+
+export const GET = async (req) => {
+    try {
+        const userId = (await getUser()).user.id
+
+        if (!userId) return NextResponse.json({ message: "You are Not Authorized" }, { status: 401 })
+
+        const select = {
+            id: true,
+            email: true,
+            username: true,
+            password: false,
+            role: true,
+            fullName: true,
+            nationalCode: true,
+            mobileNumber: true,
+            phoneNumber: true,
+            address: true,
+            postalCode: true,
+            biography: true,
+            education: true,
+            abilities: true,
+            club: true,
+            joinedAt: true,
+        }
+        const user = await prisma.user.findUnique({ where: { id: userId }, select })
+
+        return NextResponse.json({ user }, { status: 200 });
+    } catch (error) {
+        return NextResponse.json({ message: "GET ME ERROR", error }, { status: 500 })
+    }
+}
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,7 +1,204 @@
-import React from 'react'
+import { FormControl, Grid } from '@mui/material';
+import { headers } from 'next/headers';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
-export default function Profile() {
+const getMe = async () => {
+    try {
+        const resUser = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/user/getMe`, { cache: 'no-store', method: 'GET', headers: headers() });
+
+        if (!resUser.ok) {
+            throw new Error('Failed to fetch data');
+        }
+
+        const user = await resUser.json();
+        return user.user;
+    } catch (error) {
+        return {}
+    }
+}
+
+export default async function Profile() {
+    const user = await getMe()
+
+    if (!user.id)
+        redirect('/authentication')
+
     return (
-        <div>Profile</div>
+        <FormControl className="w-full">
+            <Grid container spacing={2} className='max-w-xl mx-auto m-3'>
+
+                {
+                    user.role === 'clubBoss' ?
+                        <Link href='/clubBoss'>
+                            ورود به پنل مدیریت
+                        </Link>
+                        :
+                        user.role === 'Admin' &&
+                        <Link href='/ADMIN'>
+                            ورود به پنل مدیریت
+                        </Link>
+                }
+
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        نام کاربری
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user.username}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        ایمیل
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user.email}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        نام کامل
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user.fullName}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        کد ملی
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user.nationalCode}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        شماره همراه
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user.mobileNumber}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        شماره تلفن
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user.phoneNumber}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        آدرس
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user.address}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        کد پستی
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user.postalCode}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        بیوگرافی
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user.biography}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        تحصیلات
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user.education}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        توانایی ها
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user.abilities}
+                    />
+                </Grid>
+
+                <Grid item xs={12} sm={12} md={12} lg={6}>
+                    <div className='text-start text-sm mb-1'>
+                        کارگروه (ها)
+                    </div>
+                    <input
+                        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                        id="inline-store-name"
+                        type="text"
+                        disabled
+                        value={user?.club?.join(' ، ')}
+                    />
+                </Grid>
+
+            </Grid>
+
+        </FormControl>
     )
 }
