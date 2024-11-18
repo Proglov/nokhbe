96b482d940c4433fcd8e3ddb71b682da67c9649d
Proglov <dev.mohammad.parvizi@gmail.com>
2024-11-18 16:47:02 +0330

    fixed an error

--- src/components/booksAndDocs/Main.jsx
+++ src/components/booksAndDocs/Main.jsx
@@ -1,12 +1,30 @@
 'use client'
+import { useEffect, useState } from 'react'
 import InfiniteScroll from './InfiniteScroll'
 
 
-export default async function Main({ type }) {
+export default function Main({ type }) {
     const url = process.env.NEXT_PUBLIC_API_URL + '/api/' + type + '?'
     const perPage = 20
-    const res = await fetch(`${url}page=1&perPage=${perPage}`)
-    const data = await res.json()
+
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setLoading(true)
+        const getData = async () => {
+            try {
+                const res = await fetch(`${url}page=1&perPage=${perPage}`)
+                const json = await res.json()
+                setData(json);
+            } catch (error) { }
+            finally { setLoading(false) }
+        }
+        getData()
+    }, [])
+
+    if (loading)
+        return <div className='text-center w-full'>لطفا منتظر بمانید ...</div>
 
     return (
         <InfiniteScroll previousItems={data[type]} type={type} url={url} perPage={perPage} />
